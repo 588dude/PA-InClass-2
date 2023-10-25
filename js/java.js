316e3a121d21877b0d8db2fd58c78c5a292c15bc
588dude <99377480+588dude@users.noreply.github.com>
2023-10-24 20:37:23 -0400

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,8 @@
+//Grab all the "Hide Result" buttons and images
+function showContent(elementID) {
+  document.getElementById(elementID).style.visibility = "visible";
+}
+
+function hideContent(elementID) {
+  document.getElementById(elementID).style.visibility = "hidden";
+}
